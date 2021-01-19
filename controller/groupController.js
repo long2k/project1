3a599ce3c7a511bf8015f053911801324a4461ea
long2k <47776618+long2k@users.noreply.github.com>
2021-01-19 17:50:35 +0700

    update

--- controller/groupController.js
+++ controller/groupController.js
@@ -16,15 +16,13 @@ exports.infmember = async(req,res,next)=>{
     try{
     var userId = req.body.userId;
     var id_group = req.body.id_group;
-    var modifyIn= new User({
-        username : req.body.username
-    })
-         var result = await group.findById(id_group).populate({path: 'listmb'});
-          console.log(result);
-
-        //   await User.findByIdAndUpdate({_id:userId},modifyIn);
-          res.status(200).send('Successful ' + result);
-     
+    var user= req.body.username
+         var result = await group.findById(id_group).populate('listmb');
+          if(result.listmb.indexOf(userId)){
+             await User.findByIdAndUpdate({_id:userId},{$set: {username : user}});
+              res.status(200).send("Replace Completely!");
+          };
+        
     }catch(err){
         res.status(404).send('Failed');
     }
